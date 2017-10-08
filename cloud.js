fe38292eac8f5ece7ae072adca367d17011e822a
runsisi <runsisi@hust.edu.cn>
2017-10-09 00:01:34 +0800

    initial
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- cloud.js
+++ cloud.js
@@ -1,5 +1,6 @@
 var AV = require('leanengine')
 var authFunc = require('./cloudFuncs/Auth')
+var userFunc = require('./cloudFuncs/Auth/User')
 var activityFunc = require('./cloudFuncs/Activity')
 var deviceFunc = require('./cloudFuncs/Device')
 var PingppFunc = require('./cloudFuncs/Pingpp')
@@ -19,6 +20,11 @@ AV.Cloud.define('authFetchWalletInfo', authFunc.fetchWalletInfo)
 AV.Cloud.define('authFetchDealRecords', authFunc.fetchDealRecords)
 AV.Cloud.define('authVerifyIdName', authFunc.verifyIdName)
 AV.Cloud.define('wechatIsSubscribe', mpUserFuncs.isSubscribe)
+
+AV.Cloud.define('userFetch', userFunc.fetchUser);
+AV.Cloud.define('userCreate', userFunc.createUser);
+AV.Cloud.define('userDelete', userFunc.deleteUser);
+AV.Cloud.define('userUpdate', userFunc.updateUser);
 
 //设备
 AV.Cloud.define('deviceFetchDeviceInfo', deviceFunc.fetchDeviceInfo)
---
+++ cloudFuncs/Auth/User.js
@@ -0,0 +1,26 @@
+var AV = require('leanengine');
+
+async function fetchUser(req) {
+  return {};
+}
+
+async function createUser(req) {
+  return {};
+}
+
+async function deleteUser(req) {
+  return {};
+}
+
+async function updateUser(req) {
+  return {};
+}
+
+const userApi = {
+  fetchUser,
+  createUser,
+  deleteUser,
+  updateUser,
+};
+
+module.exports = userApi;
